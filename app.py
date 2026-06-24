197b4f11ce99f79295db30047bb9cd67a236aeef
Jason W <jason.wang51@education.nsw.gov.au>
2026-06-24 03:37:14 +0000

    feature: add about and projects pages with corresponding routes and templates

--- app.py
+++ app.py
@@ -8,6 +8,26 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+
+@app.route("/project1")
+def project1():
+    return render_template("project1.html")
+
+
+@app.route("/project2")
+def project2():
+    return render_template("project2.html")
+
+
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
